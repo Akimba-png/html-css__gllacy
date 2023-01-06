ac604b97214c490f8601b7b612fef33a0947732f
Oleg <akimba@gmail.com>
2023-01-06 22:49:41 +0300

    webpack: Добавляет функционал обработки шрифтов

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,13 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[contenthash].[ext]',
+        }
+      }
     ],
   },
   optimization: {
