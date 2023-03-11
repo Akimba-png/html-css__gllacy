eded307c4034dfe4faec4d36c875898cd2ddb165
Oleg Akimov <akimba@gmail.com>
2023-03-11 17:28:08 +0300

    webpack: Добавляет функционал сжатия изображений

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,30 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpe?g)/,
+        test: /\.(png|jpe?g|webp|gif)/,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              }
+            }
+          },
+        ],
         type: 'asset/resource',
         generator: {
           filename: 'img/[name].[hash][ext]',
